ca747eba35b39805c30febe8776b32d75463c75b
Flint Vivienne <sryan.08@icloud.com>
2026-07-16 18:37:26 -0400

    feat: add basic elements to index.html and refactor app.py

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
+import os
+
 from flask import Flask, render_template, session
 from scripts import helpers
 
 app = Flask(__name__)
-if 'pack_id' not in session:
-    session['pack_id'] = helpers.makePackID()
+app.secret_key = os.environ.get('SECRET_KEY', 'default_secret_key')  # Use a default secret key if not set in environment
+
+
+@app.before_request
+def ensure_pack_id():
+    if 'pack_id' not in session:
+        session['pack_id'] = helpers.makePackID()
 
 @app.route('/', methods=['GET'])
 def index():
